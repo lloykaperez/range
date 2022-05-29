bd95d2016eef5c2a5e74dfb9089abaeadaba21c4
Lloyka Pérez <andreaperezharambour@gmail.com>
2022-05-29 00:25:33 +0000

    Ejercicio Range

---
+++ range.rb
@@ -0,0 +1,21 @@
+#.include?(value) => devuelve true or false
+
+x=(1..9)
+
+puts x.include?(3)
+
+puts x.include?(12)
+
+#.last => devuelve el último objeto en el Range
+
+y=(1..30)
+
+puts y.last
+
+#.max => devuelve el valor máximo en el Range
+#.min => devuelve el valor mínimo en el Range
+
+z=(1..255)
+
+puts z.max
+puts z.min
